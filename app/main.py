8d47fcd24684c38d06687275c3f542774fa101ea
Saurabh Yadav <saurabhyadav49345252@gmail.com>
2026-06-23 12:15:21 +0530

    fix cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.api.auth import router as auth_router
 from app.api.users import router as users_router
 from app.api.tasks import router as tasks_router
@@ -16,12 +16,14 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://pulse-frontend.vercel.app"
+        "http://localhost:3001",
+        "https://pulse-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(tasks_router)
